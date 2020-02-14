02efd5e1c0c7f12d434318184895261b254256ac
docthoi <50179314+docthoi@users.noreply.github.com>
2020-02-13 22:23:45 -0500

    Initial commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php 
+    $host = "localhost";
+    $user = "root";
+    $pass = "root";
+    $dB = "ocr";
+
+    $conn = mysqli_connect($host, $user, $pass, $dB)or die("<script> alert('Connection not made!')</script>");
+?>
