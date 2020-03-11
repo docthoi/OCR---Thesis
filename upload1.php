f04a9ab39660f16be599e54ec014fe1ea890f82a
docthoi <50179314+docthoi@users.noreply.github.com>
2020-03-11 14:19:44 -0400

    test

--- upload1.php
+++ upload1.php
@@ -3,13 +3,14 @@
 if(isset($_FILES['image'])){
     $file_name = $_FILES['image']['name'];
     $file_tmp =$_FILES['image']['tmp_name'];
-var_dump($_FILES);
+
     //...declare successful upload, display image in 50% its width...   
     move_uploaded_file($file_tmp,"images/".$file_name);
     echo "<h3>Image Upload Success</h3>";
     echo '<img src="images/'.$file_name.'" style="width:50%">';
 
     // execute tesseract OCR program, saves into OCR/images directory, output a .txt file of transcribed text 
+    shell_exec('"C:\Program Files (x86)\Tesseract-OCR\tesseract" -l chi_sim "D:\\OCR\\images'.$file_name.'" out');
 
     echo "<br><h3>OCR after reading</h3><br><pre>";
     
